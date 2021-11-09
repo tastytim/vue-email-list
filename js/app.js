6fa98a36f908e7e12e4939afc64d43588149ed6b
tastytim <tastytimgm@gmail.com>
2021-11-09 16:32:09 +0100

    commit 2

--- js/app.js
+++ js/app.js
@@ -1,23 +1,26 @@
 Vue.config.devtools = true;
 
 const vueApp = new Vue({
-  el: '#app',
-  
-  data:{
-      mailList: [
-          {mail : 'mail@test'},
-          {mail : 'mail@test'},
-          {mail : 'mail@test'},
-      ]
+  el: "#app",
+  data: {
+    listaMail: []
   },
-
   methods: {
-
-
-  }
-
-
-
-
-
-})
+    getMailList() {
+     let tempList = [];
+      const url = "https://flynn.boolean.careers/exercises/api/random/mail";
+      for (let i = 1; i <= 10; i++) {
+        axios
+          .get(url)
+          .then(function (response) {
+           tempList.push({ mail : response.data.response});
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+      this.listaMail = tempList;
+      
+    },
+  },
+});
