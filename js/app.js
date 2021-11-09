1958b53301a20e7fbd3a148f25881cf4fc7e6aaa
tastytim <tastytimgm@gmail.com>
2021-11-09 14:55:51 +0100

    commit 1

---
+++ js/app.js
@@ -0,0 +1,23 @@
+Vue.config.devtools = true;
+
+const vueApp = new Vue({
+  el: '#app',
+  
+  data:{
+      mailList: [
+          {mail : 'mail@test'},
+          {mail : 'mail@test'},
+          {mail : 'mail@test'},
+      ]
+  },
+
+  methods: {
+
+
+  }
+
+
+
+
+
+})
